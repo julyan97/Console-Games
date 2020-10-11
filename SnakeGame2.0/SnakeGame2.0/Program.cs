1f0fad6a03d4a969a5cec857162f90a2acb078e4
julyan97 <julyan97@abv.bg>
2020-10-12 02:06:26 +0300

    FS

---
+++ SnakeGame2.0/SnakeGame2.0/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeGame2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SnakeEngine eng = new SnakeEngine(50, 50);
+            eng.Run();
+        }
+    }
+}
---
+++ SnakeGame2.0/SnakeGame2.0/SnakeEngine.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SnakeGame2._0
+{
+
+    public class SnakeEngine
+    {
+        //Prop
+        private int BoardSize { get; set; }
+        private List<Point> SnakeBody { get; set; }
+        private int[][] Board { get; set; }
+        private int Snake { get; set; }
+        private int Wall { get; set; }
+        private int Food { get; set; }
+        public int Speed { get; set; }
+        private int Row { get; set; }
+        private bool IsFoodReady { get; set; } = false;
+        public int CopySpeed { get; set; }
+        private ConsoleKey Key { get; set; } = ConsoleKey.RightArrow;
+        private Random rand { get; set; } = new Random();
+
+        //Ctors
+        public SnakeEngine()
+        {
+            Row = 2;
+            CopySpeed = Speed;
+            Snake = 1;
+            Food = 2;
+            Wall = 3;
+            BoardSize = 17;
+            Board = new int[BoardSize / Row][].Select(x => new int[BoardSize]).ToArray();
+            SnakeBody = new List<Point>() { new Point(1, 1) };
+            GenerateWalls();
+        }
+        public SnakeEngine(int size, int speed) : this()
+        {
+            Speed = speed;
+            CopySpeed = speed;
+            BoardSize = size;
+            Board = new int[BoardSize / Row][].Select(x => new int[BoardSize]).ToArray();
+            SnakeBody = new List<Point>() { new Point(1, 1) };
+            GenerateWalls();
+        }
+
+        //Functions
+        private void GenerateWalls()
+        {
+            Board[0] = Board[0].Select(x => 3).ToArray();
+            Board[BoardSize / Row - 1] = Board[BoardSize / Row - 1].Select(x => 3).ToArray();
+
+            for (int i = 0; i < BoardSize / 2; i++)
+            {
+                Board[i][0] = 3;
+                Board[i][BoardSize - 1] = 3;
+            }
+
+        }
+        private void PrintBoard()
+        {
+            for (int i = 0; i < BoardSize / Row; i++)
+            {
+                for (int j = 0; j < BoardSize; j++)
+                {
+                    if (Board[i][j] == Wall) Console.Write("#");
+                    else if (Board[i][j] == Snake) Console.Write("o");
+                    else if (Board[i][j] == Food) Console.Write("@");
+                    else Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+        private void MovebyKey()
+        {
+
+            switch (Key)
+            {
+                case ConsoleKey.UpArrow:
+                    Movement(-1, 0);
+                    break;
+                case ConsoleKey.DownArrow:
+                    Movement(1, 0);
+                    break;
+                case ConsoleKey.LeftArrow:
+                    Movement(0, -1);
+                    break;
+                case ConsoleKey.RightArrow:
+                    Movement(0, 1);
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void Movement(int x, int y)
+        {
+
+            SnakeBody.Add(new Point(SnakeBody.Last().X + x, SnakeBody.Last().Y + y));
+            SnakeBody.ForEach(x =>
+            {
+                Board[x.X][x.Y] = Snake;
+            });
+            if (Board[SnakeBody.Last().X + x][SnakeBody.Last().Y + y] == Food)
+            {
+                IsFoodReady = false;
+            }
+            else
+            {
+                Board[SnakeBody.First().X][SnakeBody.First().Y] = 0;
+                SnakeBody.RemoveAt(0);
+            }
+        }
+        private void GenerateFood()
+        {
+            //if (IsFoodReady == true) return;
+            //int i = rand.Next(1, BoardSize / 2 - 2);
+            //int j = rand.Next(1, BoardSize - 2);
+            //if (Board[i][j] != 0) return;
+
+            //Board[i][j] = Food;
+            //IsFoodReady = true;
+            while (true)
+            {
+                if (IsFoodReady == true) break;
+                else
+                {
+                    int i = rand.Next(3, BoardSize / 2 - 3);
+                    int j = rand.Next(3, BoardSize - 3);
+                    if (Board[i][j] == 0)
+                    {
+                        IsFoodReady = true;
+                        Board[i][j] = Food;
+                    }
+                }
+
+            }
+        }
+        public void Run()
+        {
+            //var player = new SoundPlayer();
+            //player.SoundLocation = @"ameno.wav";
+            //player.Play();
+            while (true)
+            {
+                Console.WriteLine("To PAUSE PRESS ANY KEY OTHER THEN THE ARROWS");
+                Task.Run(() =>
+                {
+                    Key = Console.ReadKey(true).Key;
+                });
+                try
+                {
+                    PrintBoard();
+                    MovebyKey();
+                    GenerateFood();
+
+                    Thread.Sleep(Speed);
+                    Console.Clear();
+                }
+                catch
+                {
+                    Console.WriteLine("Game over LOOSER");
+                    break;
+                }
+            }
+
+
+        }
+
+    }
+}
